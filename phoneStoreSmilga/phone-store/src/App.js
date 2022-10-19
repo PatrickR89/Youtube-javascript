efe7580cda2a14026b83a35d7e3994f52f5fde9e
PatrickR89 <patrickr.89sc@gmail.com>
2022-10-19 15:50:10 +0200

    #YJS-2 [Add] react-router-dom node module
    - [Add] required components
    - [Add] connect components to app via react-router-dom

--- phoneStoreSmilga/phone-store/src/App.js
+++ phoneStoreSmilga/phone-store/src/App.js
@@ -1,21 +1,26 @@
 import logo from "./logo.svg";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">col one</div>
-          <div className="col-6">
-            <span>
-              <i className="fas fa-home" />
-            </span>
-          </div>
-        </div>
-      </div>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ProductList />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/*" element={<Default />} />
+        </Routes>
+      </>
     );
   }
 }
