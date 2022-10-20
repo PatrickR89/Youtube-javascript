c37b2425a325976ac4a159d17be0448e30450506
PatrickR89 <patrickr.89sc@gmail.com>
2022-10-20 12:31:37 +0200

    #YJS-2 [Add] context to application
    - [Add] Title component

--- phoneStoreSmilga/phone-store/src/components/ProductList.js
+++ phoneStoreSmilga/phone-store/src/components/ProductList.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      // <div>
+      //   <Product />
+      // </div>
+      <>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  return <h1>{value}</h1>;
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
