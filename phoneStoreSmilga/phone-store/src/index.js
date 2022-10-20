d0f14fd2255ec60ef84f08e738a76b13d98a6d09
PatrickR89 <patrickr.89sc@gmail.com>
2022-10-20 12:31:37 +0200

    #YJS-2 [Add] context to application
    - [Add] Title component

--- phoneStoreSmilga/phone-store/src/index.js
+++ phoneStoreSmilga/phone-store/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { ProductProvider } from "./context";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ProductProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductProvider>
   </React.StrictMode>
 );
 
