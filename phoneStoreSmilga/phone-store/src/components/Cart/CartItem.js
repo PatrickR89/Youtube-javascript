3a4965a2cbce060783ff14f1c6022f115d7ca879
PatrickR89 <patrickr.89sc@gmail.com>
2022-10-22 13:02:47 +0200

    #YJS-2 [Add] CartList to Cart
    - [Add] CartItem to CartList
    - [Add] setup img, title & price UI

---
+++ phoneStoreSmilga/phone-store/src/components/Cart/CartItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CartItem = ({ item, value }) => {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return (
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          src={img}
+          style={{ width: "5rem", heigth: "5rem" }}
+          className="img-fluid"
+          alt={title}
+        />
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">product : </span> {title}
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">price : </span> {price}
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
