2b0c1a9ca8fc5a7e7e43cc1b3df8a284b8d47761
PatrickR89 <patrickr.89sc@gmail.com>
2022-10-25 20:39:20 +0200

    #YJS-2 [Add] functionality to remove single item from cart

--- phoneStoreSmilga/phone-store/src/context.js
+++ phoneStoreSmilga/phone-store/src/context.js
@@ -75,7 +75,28 @@ class ProductProvider extends Component {
 
   increment = (id) => {};
   decrement = (id) => {};
-  removeItem = (id) => {};
+  removeItem = (id) => {
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter((item) => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
+  };
   clearCart = () => {
     this.setState(
       () => {
