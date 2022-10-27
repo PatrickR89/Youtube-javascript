61eacf311d23239afcc27febfb95f45bd4a71b5d
PatrickR89 <patrickr.89sc@gmail.com>
2022-10-27 16:49:04 +0200

    #YJS-3 [Add] generate divs as "pipes"
    - [Add] animate "pipes" move to left
    - [Add] removal of pipes out of view

---
+++ simpleGame/pipe.js
@@ -0,0 +1,75 @@
+const holeHeigth = 120;
+const pipeInterval = 1500;
+const pipeSpeed = 0.75;
+let pipes = [];
+const pipeWidth = 100;
+let timeSinceLastPipe = 0;
+let passedPipesCount = 0;
+
+export function updatePipes(delta) {
+  timeSinceLastPipe += delta;
+
+  if (timeSinceLastPipe > pipeInterval) {
+    timeSinceLastPipe -= pipeInterval;
+    createPipe();
+  }
+
+  pipes.forEach((pipe) => {
+    if (pipe.left + pipeWidth < 0) {
+      passedPipesCount++;
+      return pipe.remove();
+    }
+    pipe.left = pipe.left - delta * pipeSpeed;
+  });
+}
+export function getPassedPipesCount() {
+  return passedPipesCount;
+}
+
+export function setupPipes() {
+  document.documentElement.style.setProperty("--pipe-width", pipeWidth);
+  document.documentElement.style.setProperty("--hole-height", holeHeigth);
+  timeSinceLastPipe = pipeInterval;
+}
+
+function createPipe() {
+  const pipeElem = document.createElement("div");
+  const topElem = createPipeSegment("top");
+  const bottomElem = createPipeSegment("bottom");
+  pipeElem.append(topElem);
+  pipeElem.append(bottomElem);
+  pipeElem.classList.add("pipe");
+  pipeElem.style.setProperty(
+    "--hole-top",
+    randomNumberBetween(holeHeigth * 1.5, window.innerHeight - holeHeigth * 0.5)
+  );
+  const pipe = {
+    get left() {
+      return parseFloat(
+        getComputedStyle(pipeElem).getPropertyValue("--pipe-left")
+      );
+    },
+    set left(value) {
+      pipeElem.style.setProperty("--pipe-left", value);
+    },
+
+    remove() {
+      pipes = pipes.filter((p) => p !== pipe);
+      pipeElem.remove();
+    }
+  };
+
+  pipe.left = window.innerWidth;
+  document.body.append(pipeElem);
+  pipes.push(pipe);
+}
+
+function createPipeSegment(position) {
+  const segment = document.createElement("div");
+  segment.classList.add("segment", position);
+  return segment;
+}
+
+function randomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
--- simpleGame/script.js
+++ simpleGame/script.js
@@ -1,7 +1,9 @@
 import { updateBird, setupBird, getBirdRect } from "./bird.js";
+import { updatePipes, setupPipes, getPassedPipesCount } from "./pipe.js";
 
 document.addEventListener("keypress", handleStart, { once: true });
 const title = document.querySelector("[data-title]");
+const subtitle = document.querySelector("[data-subtitle]");
 
 let lastTime;
 function updateLoop(time) {
@@ -13,13 +15,16 @@ function updateLoop(time) {
 
   const delta = time - lastTime;
   updateBird(delta);
+  updatePipes(delta);
   if (checkLose()) return handleLose();
   lastTime = time;
   window.requestAnimationFrame(updateLoop);
 }
 function handleStart() {
   title.classList.add("hide");
+  lastTime = null;
   setupBird();
+  setupPipes();
   window.requestAnimationFrame(updateLoop);
 }
 
@@ -29,4 +34,11 @@ function checkLose() {
   return outsideWorld;
 }
 
-function handleLose() {}
+function handleLose() {
+  setTimeout(() => {
+    title.classList.remove("hide");
+    subtitle.classList.remove("hide");
+    subtitle.textContent = `${getPassedPipesCount()} pipes passed`;
+    document.addEventListener("keypress", handleStart, { once: true });
+  }, 100);
+}
