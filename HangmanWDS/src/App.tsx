47bfa17a1de98d33b2ffcc7ebe5c02823bd3ab96
PatrickR89 <patrickr.89sc@gmail.com>
2023-02-13 20:33:43 +0100

    WDS Hangman - visuals
    - [ Add ] - drawing render
    - [ Add ] - text render
    - [ Add ] - basic buttons for each word
    
    Source
    
    - [ Add ] - JSON list of words provided by WDS

--- HangmanWDS/src/App.tsx
+++ HangmanWDS/src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import words from "../wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedWords, setGuessedWords] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+
   return (
-    <div>
-      <h1>Hi</h1>
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Win Lose</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
   );
 }
