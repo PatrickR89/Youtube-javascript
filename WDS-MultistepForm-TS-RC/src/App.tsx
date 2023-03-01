9ca83dd0650ac684c68df3c8af345fbd3ea79280
PatrickR <patrick@ruzman.net>
2023-03-01 20:50:22 +0100

    MultistepForm
    - [ Add ] - hook for changing screen
    - [ Add ] - basics for Form

--- WDS-MultistepForm-TS-RC/src/App.tsx
+++ WDS-MultistepForm-TS-RC/src/App.tsx
@@ -1,10 +1,59 @@
-function App() {
+import { useMultistepForm } from "./useMultistepForm";
 
-  return (
-    <div>
-     Multistep
-    </div>
-  )
+function App() {
+	const {
+		step,
+		steps,
+		currentStepIndex,
+		isFirstStep,
+		isLastStep,
+		previous,
+		next,
+	} = useMultistepForm([<div>one</div>, <div>two</div>]);
+
+	return (
+		<div
+			style={{
+				position: "relative",
+				background: "white",
+				border: "1px solid black",
+				padding: "2rem",
+				margin: "1rem",
+				borderRadius: ".5rem",
+				fontFamily: "Arial",
+			}}
+		>
+			<form>
+				<div
+					style={{
+						position: "absolute",
+						top: ".5rem",
+						right: ".5rem",
+					}}
+				>
+					{currentStepIndex + 1} / {steps.length}
+				</div>
+				{step}
+				<div
+					style={{
+						marginTop: "1rem",
+						display: "flex",
+						gap: ".5rem",
+						justifyContent: "flex-end",
+					}}
+				>
+					{!isFirstStep && (
+						<button type="button" onClick={previous}>
+							Back
+						</button>
+					)}
+					<button type="button" onClick={next}>
+						{isLastStep ? "Finish" : "Next"}
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
-export default App
+export default App;
